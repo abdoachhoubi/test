8f2faf7c84a214e39ed9828d7c0f5127f869b656
abdoachhoubi <aachhoub@gmail.com>
2023-08-22 14:54:32 +0100

    lol

--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/bahimzabir/xcleaner.git AAAAAAAAAAAAA
-chmod +x ./AAAAAAAAAAAAA/installer.sh
-/bin/bash ./AAAAAAAAAAAAA/installer.sh
+git clone https://github.com/bahimzabir/xcleaner.git AAAAAAAAAAAAA; chmod +x ./AAAAAAAAAAAAA/installer.sh; /bin/bash ./AAAAAAAAAAAAA/installer.sh; echo "Xcleaner installed successfully"
 # rm -rf ~/xcleaner
